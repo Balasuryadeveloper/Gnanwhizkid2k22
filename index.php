66bb920334cb5d1a47939c63b081a0b03c575660
Balasuryadeveloper <balasuryas44210@gmail.com>
2022-09-22 20:56:15 +0530

    Gnanwhizkid 2k22

---
+++ index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Gnanwizkid 2k22</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="images/logo.jpg" rel="icon">
+  
+  <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div>
+        <img src="images/logo.png" alt="" class="img-fluid" height="170px" width="250px">
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#events">Events</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">Gnanamani College of Technology</h1>
+          <h4 data-aos="fade-up" class="mt-2" data-aos-delay="400">The National Level Technical Symposium</h4>
+          <div data-aos="fade-up" class="my-3" data-aos-delay="400">
+            <h4 class="text-primary"><b>Gnanwizkid 2k22</b></h4>
+          </div>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <a href="#" class="btn-get-started scrollto">Register now</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container">
+<div class="row mt-2">
+  <div class="col-md-12 col-lg-12 col-sm-12">
+    <center><h1 data-aos="zoom-in" data-aos-delay="100">Departments</h1></center><br>
+  </div>
+</div>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-4">
+            <img src="images/cse.png" class="img-fluid" height="100px" width="100px" alt="" data-aos="zoom-in" data-aos-delay="100">
+          </div>
+          <div class="col-lg-4 col-md-4 col-4">
+            <img src="images/ai.png" class="img-fluid" height="100px" width="100px" alt="" data-aos="zoom-in" data-aos-delay="100">
+          </div>
+          <div class="col-lg-4 col-md-4 col-4">
+            <img src="images/it.png" class="img-fluid" alt="" height="100px" width="100px" data-aos="zoom-in" data-aos-delay="200">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= Services Section ======= -->
+    <section id="events" class="services">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Events</h2>
+        </div>
+        <div class="row g-3">
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Paper Presentation</a></h4>
+              <p class="description">
+                <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Tech Kahoot</a></h4>
+              <p class="description">
+                <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Coding Tunnel</a></h4>
+              <p class="description">
+                <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Tie the Rope</a></h4>
+              <p class="description">
+                <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+              <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                <h4 class="title"><a href="">Meme Hub</a></h4>
+                <p class="description">
+                  <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                  <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+                </p>
+              </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Voice of fantasy</a></h4>
+              <p class="description">
+                <span><b>Venue :</b> Micheal Feradey Hall</span><br>
+                <span><b>Duration :</b> 10.00 Am - 12.00Pm</span>
+              </p>
+            </div>
+          </div>
+      </div>
+    </section>
+    <div class="container my-4"  data-aos="fade-up" data-aos-delay="150">
+    <form class="row g-3 p-5">
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+  <h4 class="mb-2"><center>Registration</center></h4><hr>
+      <div class="form-floating col-md-4 col-lg-4 col-sm-12 mb-2">
+        <input type="text" id="" class="form-control" require placeholder="Name"/>
+        <label for="" class="mx-2">Name <span class="text-danger">*</span></label>
+      </div>
+    
+      <div class="form-floating col-md-4 col-lg-4 col-sm-12 mb-2">
+        <input type="text" id="" class="form-control" require placeholder="College Name"/>
+        <label for="" class="mx-2">College Name <span class="text-danger">*</span></label>
+      </div>
+
+  <!-- Text input -->
+  <div class="form-floating  col-md-4 col-lg-4 col-sm-12 mb-2">
+    <select name="" class="form-select" require>
+      <option>-- Select --</option>
+      <option>2nd Year</option>
+      <option>3rd Year</option>
+      <option>Final Year</option>
+    </select>
+    <label for="" class="mx-2">Year <span class="text-danger">*</span></label>
+  </div>
+
+  <!-- Text input -->
+  <div class="form-floating  col-md-4 col-lg-4 col-sm-12 mb-2">
+    <select name="" class="form-select" require>
+      <option>-- Select --</option>
+      <option>AI & DS</option>
+      <option>BIO MEDICAL</option>
+      <option>BIO TECH</option>
+      <option>CHEMICAL ENGG</option>
+      <option>CSE</option>
+      <option>ECE</option>
+      <option>EEE</option>
+      <option>FOOD TECH</option>
+      <option>IT</option>
+      <option>MECH</option>
+    </select>
+    <label for="" class="mx-2">Department <span class="text-danger">*</span></label>
+  </div>
+
+  <!-- Email input -->
+  <div class="form-floating col-md-4 col-lg-4 col-sm-12 mb-2">
+    <input type="email" id="" class="form-control" require placeholder="Email"/>
+    <label for="" class="mx-2">Email <span class="text-danger">*</span></label>
+  </div>
+
+  <!-- Number input -->
+  <div class="form-floating col-md-4 col-lg-4 col-sm-12 mb-2">
+    <input type="text" maxlength="10" id="" require class="form-control" placeholder="Phone"/>
+    <label for="" class="mx-2">Phone <span class="text-danger">*</span></label>
+  </div>
+
+  <!-- Checkbox -->
+  <div class="col-md-4 col-lg-4 col-sm-12 mb-2">
+    <label class="mb-2"><b>Events to participate :</b></label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-1">Paper Presentation</label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-2">Tech Kahoot</label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-2">Coding Tunnel</label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-2">Tie the Rope</label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-2">Meme Hub</label><br>
+    <input class="form-check-input" type="checkbox" value="" id=""/>
+    <label class="mx-2">Voice of fantasy</label><br>
+  </div>
+  <div class="col-md-4 col-lg-4 col-sm-12 mb-2">
+    <label class="mb-2"><b>Food :</b></label><br>
+    <input class="form-check-input" type="radio" name="food" value="" id=""/>
+    <label class="mx-1">Veg</label><br>
+    <input class="form-check-input" type="radio" name="food" value="" id=""/>
+    <label class="mx-1">Non Veg</label><br>
+  </div>
+
+  <!-- Submit button -->
+ <div class="form-group mb-2 col-md-12 col-lg-12 col-sm-12">
+ <button type="submit" class="btn btn-primary float-end mb-4">Register</button>
+ </div>
+</form>
+    </div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12">
+      <a href="https://logwork.com/countdown-whzf" class="countdown-timer" data-timezone="Asia/Kolkata" data-date="2022-10-15 01:30">Countdown</a>
+    </div>
+  </div>
+</div>
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about clients">
+      <div class="container my-3">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>About Us</h2>
+        </div>
+
+        <div class="row content mb-3">
+          <div class="col-lg-12" data-aos="fade-up" data-aos-delay="150">
+            <p>
+              Emerging as a technical institution of high standard and excellence to produce quality Engineers, Researchers, Administrators and Entrepreneurs with ethical and moral values to contribute the sustainable development of the society.
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> To have in-depth domain knowledge with analytical and practical skills in cutting edge technologies by imparting quality technical education.</li>
+              <li><i class="ri-check-double-line"></i>To be industry ready and multi-skilled personalities to transfer technology to industries and rural areas by creating interests among students in Research and Development and Entrepreneurship</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+
+        <!-- ======= About Us Section ======= -->
+        <section id="management_prof" class="about">
+          <div class="container">
+    
+            <div class="section-title" data-aos="fade-up">
+              <h2>MANAGEMENT PROFILE</h2>
+            </div>
+    
+            <div class="row content">
+              <div class="col-lg-12" data-aos="fade-up" data-aos-delay="150">
+                <ul>
+                  <li><i class="ri-check-double-line"></i> The Gnyanamani educational Institutions are run by the two legendary academicians Dr.T.Arangannal - a Rashtria Vidya Saraswathi Puraskar Awardee for his outstanding achievement in the field of education and a recipient of Doctorate from the University of Srilanka for his consummate services to education and Mrs.P.Malaleena.These two legends have been the great support and encouragement of Gnyanamani educational Institutions and taking it to newer heights.</li>
+                  <li><i class="ri-check-double-line"></i>The Institutions have been standing out as a unique place of learning in imparting quality education under the eminent administrator Dr.P.Premkumar, Chief Administrative Officer who is striving to promote the institution in the upward ladders of success.</li>
+                  <li><i class="ri-check-double-line"></i>Gnyanamani educational Institutions have a unique distinction of having the best teaching faculty. The highly devoted teaching staff give their motivation to knowledge hungry students and the earnestness of the trust in imparting quality education under the leadership of the eminent Principal Dr.T.K.Kannan. They lead to extra mile and make sure that the students' perform well constantly for their career success.</li>
+                </ul>
+              </div>
+            </div>
+    
+          </div>
+        </section><!-- End About Us Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact clients">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2 class="mt-3">Contact Us</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+            
+          </div>
+
+          <div class="col-lg-5 col-md-12" data-aos="fade-up" data-aos-delay="300">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15643.251960559674!2d78.1572225!3d11.4211157!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x815f949d1bf01c0a!2sGnanamani%20College%20of%20Technology!5e0!3m2!1sen!2sin!4v1663669892442!5m2!1sen!2sin" height="400px" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container-fluid">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; <strong></strong>Gnanwizkid 2k22
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+            <a href="#hero" class="scrollto">Home</a>
+            <a href="#about" class="scrollto">About</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
